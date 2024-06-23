bcd1b1764e464b530e2e0b106a871f24411450f1
mostafaalimorsy <mostafamorsy501@gmail.com>
2024-06-23 10:11:26 +0300

    update: firebase crachlystics

--- apps/shorebirdtest/lib/main.dart
+++ apps/shorebirdtest/lib/main.dart
@@ -1,6 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:shorebirdtest/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const MyApp());
 }
 
@@ -117,7 +133,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () => throw Exception(),
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
--- packages/shared/testshreadpackages/lib/testshreadpackages.dart
+++ packages/shared/testshreadpackages/lib/testshreadpackages.dart
@@ -5,3 +5,7 @@ class Calculator {
   /// Returns [value] plus 1.
   static int addOne() => 1;
 }
+// CI CD
+// firebase crashlytics
+// TDD (integration)
+
